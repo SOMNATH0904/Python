a9c0168c34037512eea2ae552468671a0a70d3d7
SOMNATH0904 <somnathshaw900@gmail.com>
2024-08-11 17:31:32 +0530

    Updated a Simple Calculator using Python

---
+++ 10. Exercises/01. CalculatorUsingPython.py
@@ -0,0 +1,9 @@
+a = int(input("Enter 1st Number :"))
+b = int(input("Enter 2nd Number :"))
+
+print("The vakye of", a, "+", b, "is :", (a - b))
+print("The vakye of", a, "-", b, "is :", (a + b))
+print("The vakye of", a, "*", b, "is :", (a * b))
+print("The vakye of", a, "/", b, "is :", (a / b))
+print("The vakye of", a, "%", b, "is :", (a % b))
+print("The vakye of", a, "**", b, "is :", (a ** b))
