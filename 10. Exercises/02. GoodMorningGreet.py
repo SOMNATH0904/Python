a4d63b0a3b75db30e4ceb94597b10a2abe928739
SOMNATH0904 <somnathshaw900@gmail.com>
2024-08-11 19:09:57 +0530

    Updated Simple Greeting code

---
+++ 10. Exercises/02. GoodMorningGreet.py
@@ -0,0 +1,15 @@
+'''
+Create a python program capable of greeting you with Good Morning, Good Afternoon and Good Evening. Your program
+should use time module to get the current hour.
+'''
+
+import time
+t = time.strftime('%H:%M:%S')
+hour = int(time.strftime('%H'))
+
+if(hour >= 0 and hour < 12):
+    print("Good Morning !!")
+elif(hour >= 12 and hour < 17):
+    print("Good Afternoon !!")
+elif(hour >= 17 and hour < 0):
+    print("Good Night !!")
